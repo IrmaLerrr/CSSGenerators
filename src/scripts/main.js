b91bb627b9591e921129cc3d3965e567b5a559fd
IrinaLazarenko <irma.lerrr.wine@gmail.com>
2024-09-12 17:27:28 +0300

    Add gradient generator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,41 @@
 $(document).ready(function () {
   //
-  $("#mode-btn").on("click", function () {
+  $("select").on("change", function () {
+    changeSelect();
+  });
+
+  function changeSelect() {
+    switch ($('select').val()) {
+      case "bsg":
+        $(".bsg").css("display", "block");
+        $(".tsg").css("display", "none");
+        $(".gg").css("display", "none");
+        break;
+      case "tsg":
+        $(".tsg").css("display", "block");
+        $(".bsg").css("display", "none");
+        $(".gg").css("display", "none");
+        break;
+      case "gg":
+        $(".tsg").css("display", "none");
+        $(".bsg").css("display", "none");
+        $(".gg").css("display", "block");
+        break;
+    }
+  }
+  changeSelect();
+
+  $("#btn_mode").on("click", function () {
     if ($(this).attr("mode") == "day") {
       $(this).attr("mode", "night");
       $(this).text("night mode");
       $("body").css("background-color", "#333333");
-      $("h1").css("color", "#ffffff");
+      $(".tsg_title").css("color", "#ffffff");
     } else {
       $(this).attr("mode", "day");
       $(this).text("day mode");
       $("body").css("background-color", "#dddddd");
-      $("h1").css("color", "#000000");
+      $(".tsg_title").css("color", "#000000");
     }
   });
   //
