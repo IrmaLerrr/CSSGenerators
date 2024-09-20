2ba1bd5809c9cc946933da2855a696708e4d8056
IrinaLazarenko <irma.lerrr.wine@gmail.com>
2024-09-20 20:19:17 +0300

    add functional of multiply shadow for tsg and bsg

--- src/components/box-shadow-generator/box-shadow-generator.js
+++ src/components/box-shadow-generator/box-shadow-generator.js
@@ -1,12 +1,23 @@
-var shadowObj = {
-  units: "px",
-  "bsg_range-offset-X": $("#bsg_range-offset-X").val(),
-  "bsg_range-offset-Y": $("#bsg_range-offset-Y").val(),
-  "bsg_range-blur": $("#bsg_range-blur").val(),
-  "bsg_range-spread": $("#bsg_range-spread").val(),
-  bsg_colorpick: $("#bsg_colorpick").val(),
-  "bsg_range-opacity": $("#bsg_range-opacity").val(),
-};
+var shadowObj = {};
+function changeShadowObj() {
+  shadowObj = {
+    units: "px",
+    count: $(".shadow-card").length,
+    "bsg_range-font-size": $("#bsg_range-font-size").val(),
+    "bsg_colorpick-box": $("#bsg_colorpick-box").val(),
+    "bsg_colorpick-text": $("#bsg_colorpick-text").val(),
+  };
+  for (let i = 1; i <= shadowObj.count; i++) {
+    shadowObj["bsg_radio_type" + i] = $(`input[name='bsg_radio_type${i}']:checked`).val();
+    shadowObj["bsg_range-offset-X" + i] = $(`#bsg_range-offset-X${i}`).val();
+    shadowObj["bsg_range-offset-Y" + i] = $(`#bsg_range-offset-Y${i}`).val();
+    shadowObj["bsg_range-blur" + i] = $(`#bsg_range-blur${i}`).val();
+    shadowObj["bsg_range-spread" + i] = $(`#bsg_range-spread${i}`).val();
+    shadowObj["bsg_colorpick" + i] = $(`#bsg_colorpick${i}`).val();
+    shadowObj["bsg_range-opacity" + i] = $(`#bsg_range-opacity${i}`).val();
+  }
+}
+changeShadowObj();
 
 function getColorhex(color, opacity) {
   let colorhex =
@@ -19,7 +30,7 @@ function getColorhex(color, opacity) {
 
 function getColorrgb(color, opacity) {
   let colorrgb =
-    " rgb(" +
+    "rgb(" +
     parseInt(color.slice(1, 3), 16) +
     ", " +
     parseInt(color.slice(3, 5), 16) +
@@ -30,40 +41,49 @@ function getColorrgb(color, opacity) {
     ")";
   return colorrgb;
 }
-
-function getBoxShadow(units, array, color) {
+function getBoxShadow(color) {
   let shadow = "";
-  for (let i = 0; i < array.length; i++) {
-    shadow = shadow + array[i] + units + " ";
+  for (let i = 1; i <= shadowObj.count; i++) {
+    shadow =
+      shadow +
+      shadowObj["bsg_range-offset-X" + i] +
+      shadowObj.units +
+      " " +
+      shadowObj["bsg_range-offset-Y" + i] +
+      shadowObj.units +
+      " " +
+      shadowObj["bsg_range-blur" + i] +
+      shadowObj.units +
+      " " +
+      shadowObj["bsg_range-spread" + i] +
+      shadowObj.units +
+      " " +
+      (color === "HEX"
+        ? getColorhex(
+            shadowObj["bsg_colorpick" + i],
+            shadowObj["bsg_range-opacity" + i]
+          )
+        : getColorrgb(
+            shadowObj["bsg_colorpick" + i],
+            shadowObj["bsg_range-opacity" + i]
+          )) +
+      (shadowObj["bsg_radio_type" + i] === "True" ? " inset" : "") +
+      ", ";
   }
-  shadow = shadow + color;
+  shadow = shadow.slice(0, -2);
   return shadow;
 }
 
-function changeTextShadow() {
+function changeBoxShadow() {
+  var shadowhex = getBoxShadow("HEX");
+  var shadowrgb = getBoxShadow("RGB");
 
-  var shadowhex = getBoxShadow(
-    shadowObj.units,
-    [
-      shadowObj["bsg_range-offset-X"],
-      shadowObj["bsg_range-offset-Y"],
-      shadowObj["bsg_range-blur"],
-      shadowObj["bsg_range-spread"],
-    ],
-    getColorhex(shadowObj["bsg_colorpick"], shadowObj["bsg_range-opacity"])
+  $(".bsg_title").css(
+    "font-size",
+    shadowObj["bsg_range-font-size"] + shadowObj.units
   );
-  var shadowrgb = getBoxShadow(
-    shadowObj.units,
-    [
-      shadowObj["bsg_range-offset-X"],
-      shadowObj["bsg_range-offset-Y"],
-      shadowObj["bsg_range-blur"],
-      shadowObj["bsg_range-spread"],
-    ],
-    getColorrgb(shadowObj["bsg_colorpick"], shadowObj["bsg_range-opacity"])
-  );
-
-
+  $(".bsg_title").css("background-color", shadowObj["bsg_colorpick-box"]);
+  $(".bsg_title").css("color", shadowObj["bsg_colorpick-text"]);
   $(".bsg_title").css("box-shadow", shadowhex);
 
   var resulthex = `{\n  box-shadow: ${shadowhex};\n}`;
@@ -72,11 +92,80 @@ function changeTextShadow() {
   $("#bsg_resulthex").val(resulthex);
   $("#bsg_resultrgb").val(resultrgb);
 }
+changeBoxShadow();
 
-changeTextShadow();
-
-$(".range, .colorpick").on("input", function () {
+$(document).on("input", ".range, .colorpick", function () {
   shadowObj[$(this).attr("id")] = $("#" + $(this).attr("id")).val();
   $(this).next().children(":first").text($(this).val());
-  changeTextShadow();
+  changeBoxShadow();
 });
+$(document).on("input", ".radio", function () {
+  for (let i = 1; i <= shadowObj.count; i++) {
+    shadowObj["bsg_radio_type" + i] = $(`input[name='bsg_radio_type${i}']:checked`).val();
+    // alert()
+  }
+  changeBoxShadow();
+});
+
+$(document).on("click", ".bsg_add-btn", function () {
+  $(".bsg_result-card").before(
+    $(this).parent().parent().parent().parent().clone()
+  );
+  reassignID();
+  changeShadowObj();
+  changeBoxShadow();
+});
+$(document).on("click", ".bsg_delete-btn", function () {
+  $(this).parent().parent().parent().parent().remove();
+  reassignID();
+  changeShadowObj();
+  changeBoxShadow();
+});
+
+function reassignID() {
+  $(".shadow-card").each(function (index) {
+    index++;
+    $(this)
+      .find(".card-header")
+      .text("Shadow " + index);
+    $(this)
+      .find(".radio")
+      .attr("name", "bsg_radio_type" + index)
+      .attr("title", "bsg_radio_type" + index);
+    $(this)
+      .find(".radio[value='False']")
+      .prop("checked", true);
+    $(this)
+      .find(".range-offset-X")
+      .attr("id", "bsg_range-offset-X" + index)
+      .attr("title", "#bsg_range-offset-X" + index);
+    $(this)
+      .find(".range-offset-Y")
+      .attr("id", "bsg_range-offset-Y" + index)
+      .attr("title", "#bsg_range-offset-Y" + index);
+    $(this)
+      .find(".range-blur")
+      .attr("id", "bsg_range-blur" + index)
+      .attr("title", "#bsg_range-blur" + index);
+    $(this)
+      .find(".range-spread")
+      .attr("id", "bsg_range-spread" + index)
+      .attr("title", "#bsg_range-spread" + index);
+    $(this)
+      .find(".colorpick")
+      .attr("id", "bsg_colorpick" + index)
+      .attr("title", "#bsg_colorpick" + index);
+    $(this)
+      .find(".range-opacity")
+      .attr("id", "bsg_range-opacity" + index)
+      .attr("title", "#bsg_range-opacity" + index);
+    $(this)
+      .find(".bsg_add-btn")
+      .attr("id", "bsg_add-shadow" + index)
+      .attr("title", "bsg_add-shadow" + index);
+    $(this)
+      .find(".bsg_delete-btn")
+      .attr("id", "bsg_delete-shadow" + index)
+      .attr("title", "bsg_delete-shadow" + index);
+  });
+}
--- src/components/text-shadow-generator/text-shadow-generator.js
+++ src/components/text-shadow-generator/text-shadow-generator.js
@@ -1,12 +1,21 @@
-var shadowObj = {
-  units: "px",
-  "tsg_range-font-size": $("#tsg_range-font-size").val(),
-  "tsg_range-offset-X": $("#tsg_range-offset-X").val(),
-  "tsg_range-offset-Y": $("#tsg_range-offset-Y").val(),
-  "tsg_range-blur": $("#tsg_range-blur").val(),
-  tsg_colorpick: $("#tsg_colorpick").val(),
-  "tsg_range-opacity": $("#tsg_range-opacity").val(),
-};
+var shadowObj = {};
+function changeShadowObj() {
+  shadowObj = {
+    units: "px",
+    count: $(".tsg_shadow-card").length,
+    "tsg_range-font-size": $("#tsg_range-font-size").val(),
+    "tsg_colorpick-text": $("#tsg_colorpick-text").val(),
+  };
+  for (let i = 1; i <= shadowObj.count; i++) {
+    shadowObj["tsg_range-offset-X" + i] = $(`#tsg_range-offset-X${i}`).val();
+    shadowObj["tsg_range-offset-Y" + i] = $(`#tsg_range-offset-Y${i}`).val();
+    shadowObj["tsg_range-blur" + i] = $(`#tsg_range-blur${i}`).val();
+    shadowObj["tsg_colorpick" + i] = $(`#tsg_colorpick${i}`).val();
+    shadowObj["tsg_range-opacity" + i] = $(`#tsg_range-opacity${i}`).val();
+  }
+}
+changeShadowObj();
+
 function getColorhex(color, opacity) {
   let colorhex =
     color.toUpperCase() +
@@ -18,7 +27,7 @@ function getColorhex(color, opacity) {
 
 function getColorrgb(color, opacity) {
   let colorrgb =
-    " rgb(" +
+    "rgb(" +
     parseInt(color.slice(1, 3), 16) +
     ", " +
     parseInt(color.slice(3, 5), 16) +
@@ -29,47 +38,181 @@ function getColorrgb(color, opacity) {
     ")";
   return colorrgb;
 }
-
-function getShadow(units, offsetX, offsetY, blur, color) {
-  let shadow =
-    offsetX + units + " " + offsetY + units + " " + blur + units + " " + color;
+function getBoxShadow(color) {
+  let shadow = "";
+  for (let i = 1; i <= shadowObj.count; i++) {
+    shadow =
+      shadow +
+      shadowObj["tsg_range-offset-X" + i] +
+      shadowObj.units +
+      " " +
+      shadowObj["tsg_range-offset-Y" + i] +
+      shadowObj.units +
+      " " +
+      shadowObj["tsg_range-blur" + i] +
+      shadowObj.units +
+      " " +
+      (color === "HEX"
+        ? getColorhex(
+            shadowObj["tsg_colorpick" + i],
+            shadowObj["tsg_range-opacity" + i]
+          )
+        : getColorrgb(
+            shadowObj["tsg_colorpick" + i],
+            shadowObj["tsg_range-opacity" + i]
+          )) +
+      ", ";
+  }
+  shadow = shadow.slice(0, -2);
   return shadow;
 }
 
-function changeTextShadow() {
-  var shadowhex = getShadow(
-    shadowObj.units,
-    shadowObj["tsg_range-offset-X"],
-    shadowObj["tsg_range-offset-Y"],
-    shadowObj["tsg_range-blur"],
-    getColorhex(shadowObj["tsg_colorpick"], shadowObj["tsg_range-opacity"])
-  );
-  var shadowrgb = getShadow(
-    shadowObj.units,
-    shadowObj["tsg_range-offset-X"],
-    shadowObj["tsg_range-offset-Y"],
-    shadowObj["tsg_range-blur"],
-    getColorrgb(shadowObj["tsg_colorpick"], shadowObj["tsg_range-opacity"])
-  );
+function changeBoxShadow() {
+  var shadowhex = getBoxShadow("HEX");
+  var shadowrgb = getBoxShadow("RGB");
 
   $(".tsg_title").css("font-size", shadowObj["tsg_range-font-size"] + shadowObj.units);
+  $(".tsg_title").css("color", shadowObj["tsg_colorpick-text"]);
   $(".tsg_title").css("text-shadow", shadowhex);
 
-  var resulthex = `{\n  font-size: ${
-    shadowObj["tsg_range-font-size"] + shadowObj.units
-  };\n  text-shadow: ${shadowhex};\n}`;
-  var resultrgb = `{\n  font-size: ${
-    shadowObj["tsg_range-font-size"] + shadowObj.units
-  };\n  text-shadow: ${shadowrgb};\n}`;
+  var resulthex = `{\n  text-shadow: ${shadowhex};\n}`;
+  var resultrgb = `{\n  text-shadow: ${shadowrgb};\n}`;
 
   $("#tsg_resulthex").val(resulthex);
   $("#tsg_resultrgb").val(resultrgb);
 }
+changeBoxShadow();
 
-changeTextShadow();
-
-$(".range, #tsg_colorpick").on("input", function () {
+$(document).on("input", ".range, .colorpick", function () {
   shadowObj[$(this).attr("id")] = $("#" + $(this).attr("id")).val();
   $(this).next().children(":first").text($(this).val());
-  changeTextShadow();
+  changeBoxShadow();
 });
+
+$(document).on("click", ".tsg_add-btn", function () {
+  $(".tsg_result-card").before(
+    $(this).parent().parent().parent().parent().clone()
+  );
+  reassignID();
+  changeShadowObj();
+  changeBoxShadow();
+});
+$(document).on("click", ".tsg_delete-btn", function () {
+  $(this).parent().parent().parent().parent().remove();
+  reassignID();
+  changeShadowObj();
+  changeBoxShadow();
+});
+
+function reassignID() {
+  $(".tsg_shadow-card").each(function (index) {
+    index++;
+    $(this)
+      .find(".card-header")
+      .text("Shadow " + index);
+    $(this)
+      .find(".range-offset-X")
+      .attr("id", "tsg_range-offset-X" + index)
+      .attr("title", "#tsg_range-offset-X" + index);
+    $(this)
+      .find(".range-offset-Y")
+      .attr("id", "tsg_range-offset-Y" + index)
+      .attr("title", "#tsg_range-offset-Y" + index);
+    $(this)
+      .find(".range-blur")
+      .attr("id", "tsg_range-blur" + index)
+      .attr("title", "#tsg_range-blur" + index);
+    $(this)
+      .find(".colorpick")
+      .attr("id", "tsg_colorpick" + index)
+      .attr("title", "#tsg_colorpick" + index);
+    $(this)
+      .find(".range-opacity")
+      .attr("id", "tsg_range-opacity" + index)
+      .attr("title", "#tsg_range-opacity" + index);
+    $(this)
+      .find(".add-btn")
+      .attr("id", "tsg_add-shadow" + index)
+      .attr("title", "tsg_add-shadow" + index);
+    $(this)
+      .find(".delete-btn")
+      .attr("id", "tsg_delete-shadow" + index)
+      .attr("title", "tsg_delete-shadow" + index);
+  });
+}
+
+// var shadowObj = {
+//   units: "px",
+//   "tsg_range-font-size": $("#tsg_range-font-size").val(),
+//   "tsg_range-offset-X": $("#tsg_range-offset-X").val(),
+//   "tsg_range-offset-Y": $("#tsg_range-offset-Y").val(),
+//   "tsg_range-blur": $("#tsg_range-blur").val(),
+//   tsg_colorpick: $("#tsg_colorpick").val(),
+//   "tsg_range-opacity": $("#tsg_range-opacity").val(),
+// };
+// function getColorhex(color, opacity) {
+//   let colorhex =
+//     color.toUpperCase() +
+//     Math.floor(opacity * 255)
+//       .toString(16)
+//       .toUpperCase();
+//   return colorhex;
+// }
+
+// function getColorrgb(color, opacity) {
+//   let colorrgb =
+//     " rgb(" +
+//     parseInt(color.slice(1, 3), 16) +
+//     ", " +
+//     parseInt(color.slice(3, 5), 16) +
+//     ", " +
+//     parseInt(color.slice(5, 7), 16) +
+//     ", " +
+//     opacity +
+//     ")";
+//   return colorrgb;
+// }
+
+// function getShadow(units, offsetX, offsetY, blur, color) {
+//   let shadow =
+//     offsetX + units + " " + offsetY + units + " " + blur + units + " " + color;
+//   return shadow;
+// }
+
+// function changeTextShadow() {
+//   var shadowhex = getShadow(
+//     shadowObj.units,
+//     shadowObj["tsg_range-offset-X"],
+//     shadowObj["tsg_range-offset-Y"],
+//     shadowObj["tsg_range-blur"],
+//     getColorhex(shadowObj["tsg_colorpick"], shadowObj["tsg_range-opacity"])
+//   );
+//   var shadowrgb = getShadow(
+//     shadowObj.units,
+//     shadowObj["tsg_range-offset-X"],
+//     shadowObj["tsg_range-offset-Y"],
+//     shadowObj["tsg_range-blur"],
+//     getColorrgb(shadowObj["tsg_colorpick"], shadowObj["tsg_range-opacity"])
+//   );
+
+//   $(".tsg_title").css("font-size", shadowObj["tsg_range-font-size"] + shadowObj.units);
+//   $(".tsg_title").css("text-shadow", shadowhex);
+
+//   var resulthex = `{\n  font-size: ${
+//     shadowObj["tsg_range-font-size"] + shadowObj.units
+//   };\n  text-shadow: ${shadowhex};\n}`;
+//   var resultrgb = `{\n  font-size: ${
+//     shadowObj["tsg_range-font-size"] + shadowObj.units
+//   };\n  text-shadow: ${shadowrgb};\n}`;
+
+//   $("#tsg_resulthex").val(resulthex);
+//   $("#tsg_resultrgb").val(resultrgb);
+// }
+
+// changeTextShadow();
+
+// $(".range, #tsg_colorpick").on("input", function () {
+//   shadowObj[$(this).attr("id")] = $("#" + $(this).attr("id")).val();
+//   $(this).next().children(":first").text($(this).val());
+//   changeTextShadow();
+// });
